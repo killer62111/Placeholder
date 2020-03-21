b4d5a660d06e8beeb879ebc6787a5c03cb15b10b
killer62111 <killergaming62111@gmail.com>
2020-03-21 23:45:20 +0100

    1er changement bot

---
+++ main.js
@@ -0,0 +1,13 @@
+const { Client } = require("discord.js");
+const { TOKEN, PREFIX } = require("./config");
+const client = new Client({ disableEveryone: true });
+
+client.on("ready", () => {
+  console.log("Je suis prêt !");
+});
+
+client.on("message", msg => {
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong!");
+});
+
+client.login(TOKEN);
